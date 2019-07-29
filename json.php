18030ac0932508a88949e04e9c3e26dcb8927f2c
Lisonallie <allisonvanlinden@gmail.com>
2019-07-29 16:58:05 +0200

    try to add changed url from htaccess

--- json.php
+++ json.php
@@ -1,11 +1,13 @@
 <?php
+    header("Content-type:application/json");
+    header("Access-Control-Allow-Origin: *");
     $randomnum = [];
     
-    for ($i = 0; $i < 11; $i++) {
+    for ($i = 0; $i < $_GET['numbers']; $i++) {
         $yvalue = mt_rand(-5, 20);
          $json = [$i, $yvalue];
          array_push($randomnum, $json);
         }
-        echo json_encode($randomnum);
-        header("Content-type:application/json");
+    echo json_encode($randomnum);
+
 ?>
