58ae8c349a3c4da013d92c026b0b0ffb68ad745a
Lisonallie <allisonvanlinden@gmail.com>
2019-07-29 12:05:26 +0200

    implement for loop

--- json.php
+++ json.php
@@ -1,4 +1,9 @@
 <?php
     $yvalue = mt_rand(-20, 20);
     $randomnum = [];
+
+    for ($i = 0; $i < $randomnum.length; $i++) {
+         $json = "[$i, $yvalue]";
+         $randomnum.push($json);
+    }
 ?>
