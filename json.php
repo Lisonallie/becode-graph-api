b4a7182a5f2bb8573578920c8ac5fd14f26e1e13
Lisonallie <allisonvanlinden@gmail.com>
2019-08-07 12:30:44 +0200

    fix broken file

--- json.php
+++ json.php
@@ -3,11 +3,10 @@
     header("Access-Control-Allow-Origin: *");
     $randomnum = [];
     
-    for ($i = 0; $i < $_GET['numbers']; $i++) {
+    for ($i = 0; $i < 11; $i++) {
         $yvalue = mt_rand(-5, 20);
          $json = [$i, $yvalue];
          array_push($randomnum, $json);
         }
     echo json_encode($randomnum);
-
 ?>
